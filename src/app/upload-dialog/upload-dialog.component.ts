811731fc542a2d9fa701238773be54ce34739ab0
chenjingorz <chenjingorz@users.noreply.github.com>
2020-12-14 00:27:27 +0800

    adding upload functionality to mongodb

--- src/app/upload-dialog/upload-dialog.component.ts
+++ src/app/upload-dialog/upload-dialog.component.ts
@@ -12,6 +12,7 @@ export class UploadDialogComponent {
   @ViewChild('fileInput') fileInput: ElementRef;
   fileAttr = 'Choose File';
   csvFile: any;
+  JSONData: any;
 
   constructor( public dialogRef: MatDialogRef<UploadDialogComponent>,
                private _snackBar: MatSnackBar,
@@ -21,18 +22,19 @@ export class UploadDialogComponent {
     this.dialogRef.close();
   }
 
-  // send(): void{
-  //   console.log('send');
-  //   // call the api to send the file. when the get the reply show toast
-  //   this.RestService.uploadCSV(this.csvFile)
-  //     .subscribe(response => {
-  //     if (response === '200'){
-  //       this.openSnackBar('file uploaded success', 'close');
-  //       this.closeModal();
-  //     } else {
-  //       this.openSnackBar('file uploaded failed', 'close');
-  //     }});
-  // }
+  send(): void{
+    console.log('send');
+    // call the api to send the file. when the get the reply show toast
+    this.RestService.uploadCSV(this.csvFile)
+      .subscribe(response => {
+      if (response === 200){
+        this.openSnackBar('file uploaded success', 'close');
+        this.closeModal();
+      } else {
+        this.openSnackBar('file uploaded failed', 'close');
+      }
+    });
+  }
 
 
   openSnackBar(message: string, action: string): void{
@@ -40,22 +42,13 @@ export class UploadDialogComponent {
   }
 
   csvInputChange(fileInput: any): void{
-    // console.log(fileInput.target.files[0]);
-    this.csvFile = fileInput.target.files[0];
-    if (fileInput.target.files && fileInput.target.files[0]) {
-      this.fileAttr = '';
-      // for multiple files selected
-      Array.from(fileInput.target.files).forEach((file: File) => {
-        this.fileAttr += file.name + ' - ';
-      });
-
-      // HTML5 FileReader API
-      const reader = new FileReader();
-      reader.readAsDataURL(fileInput.target.files[0]);
-
+    console.log(fileInput);
+    if (fileInput.target.files) {
+      this.fileAttr = fileInput.target.files[0].name;
+      const file: File = fileInput.target.files[0];
+      this.csvFile = file;
     } else {
       this.fileAttr = 'Choose File';
     }
-    this.openSnackBar('File uploaded!','Close')
   }
 }
